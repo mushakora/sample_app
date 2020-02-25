4a37221605bb8f3343175c986f4bd26e5acba2fb
mushakora <mushakora89br@yahoo.co.jp>
2020-02-25 21:24:51 +0900

    rails f

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6fd321190d15b8ff1a3c04b27ef0ed29ffacddf8371a56680c06d0aff4787c1eab06f0711683e542bd82017e46570515ddb8e2688dec14aa5c7974d204f68d4c'
